e61f463f566782dd4c8f394e6da52cd0e8740406
morifumiya <morimorimorifu@gmail.com>
2019-09-04 10:40:07 +0000

    課題１１

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,17 @@
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- admin.blade.phpの@yield('tittle')に'ニュースの新規作成'を埋め込む --}}
+@section('tittle', 'Profile新規作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Profileの新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+    
+});
+Route::group(['prefix' => 'admin'], function() {
+  Route::get('news/create', 'Admin\NewsController@add');
+  Route::get('profile/create', 'Admin\ProfileController@add');
+  Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
